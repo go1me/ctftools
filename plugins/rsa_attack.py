af7876d2e2699c4ec3d4d49dafb2bcf60ce8be46
haisheng zhou <ddugye@gmail.com>
2020-05-03 21:48:16 +0800

    rsa_attack

---
+++ plugins/rsa_attack.py
@@ -0,0 +1,14 @@
+import binascii
+plugin_name = "rsa各种攻击方法参考"
+plugin_version = "v1.1"
+plugin_author = "1me"
+plugin_time = "20200429"
+plugin_info = '''rsa各种攻击方法参考
+https://blog.csdn.net/weixin_41038905/article/details/105827655
+https://findneo.github.io/180727rsa-attack/
+https://github.com/findneo/RSA-ATTACK
+'''
+
+
+def run(s):
+    return plugin_info
