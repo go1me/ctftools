d2d22e2bebe1bc2ae76559a7ee90423c3651d958
haysengithub <dduguye@gmail.com>
2020-05-04 23:49:56 +0800

    增加测试用例功能

--- plugins/base64_encode.py
+++ plugins/base64_encode.py
@@ -3,6 +3,8 @@ plugin_name = "base64加密"
 plugin_version = "v1.1"
 plugin_author = "1me"
 plugin_time = "20200429"
+plugin_test_in="flag"#测试用例的输入
+plugin_test_out="ZmxhZw=="#测试用例的输出
 plugin_info = "base64加密"
 
 
